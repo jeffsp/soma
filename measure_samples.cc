1bf22c604235682f2b67ea89bdaa9e7a167e64cb
Jeff Perry <jeffsp@gmail.com>
2013-09-11 15:34:06 -0500

    add stats for getting distance and angles between two points

--- measure_samples.cc
+++ measure_samples.cc
@@ -12,36 +12,68 @@ using namespace soma;
 const string usage = "usage: measure_samples";
 
 template<typename W>
-void stats (const W &w)
+void stats (const W &w, size_t index)
 {
     auto s = w.get_samples ();
     vector<float> x;
     vector<float> y;
     vector<float> z;
-    vector<float> d;
+    vector<float> a;
     for (size_t i = 0; i + 1 < s.size (); ++i)
     {
-        if (s[i].size () == 1)
+        if (s[i].size () > index)
         {
-            float fx = s[i][0].x;
-            float fy = s[i][0].y;
-            float fz = s[i][0].z;
+            float fx = s[i][index].x;
+            float fy = s[i][index].y;
+            float fz = s[i][index].z;
             x.push_back (fx);
             y.push_back (fy);
             z.push_back (fz);
-            d.push_back (sqrt (fx * fx + fy * fy + fz * fz));
+            a.push_back (sqrt (fx * fx + fy * fy + fz * fz));
         }
     }
     clog
-        << "average, stddev "
         << ' ' << average (x)
         << ' ' << average (y)
         << ' ' << average (z)
-        << ' ' << average (d)
+        << ' ' << average (a)
         << ' ' << sqrt (variance (x))
         << ' ' << sqrt (variance (y))
         << ' ' << sqrt (variance (z))
+        << ' ' << sqrt (variance (a))
+        << endl;
+}
+
+template<typename W>
+void stats (const W &w)
+{
+    auto s = w.get_samples ();
+    vector<float> d;
+    vector<float> xy;
+    vector<float> yz;
+    vector<float> zx;
+    for (size_t i = 0; i + 1 < s.size (); ++i)
+    {
+        if (s[i].size () == 2)
+        {
+            float dx = s[i][0].x - s[i][1].x;
+            float dy = s[i][0].y - s[i][1].y;
+            float dz = s[i][0].z - s[i][1].z;
+            d.push_back (sqrt (dx * dx + dy * dy + dz * dz));
+            xy.push_back (atan (dx / dy) * 180 / M_PI);
+            yz.push_back (atan (dy / dz) * 180 / M_PI);
+            zx.push_back (atan (dz / dx) * 180 / M_PI);
+        }
+    }
+    clog
+        << ' ' << average (d)
+        << ' ' << average (xy)
+        << ' ' << average (yz)
+        << ' ' << average (zx)
         << ' ' << sqrt (variance (d))
+        << ' ' << sqrt (variance (xy))
+        << ' ' << sqrt (variance (yz))
+        << ' ' << sqrt (variance (zx))
         << endl;
 }
 
@@ -84,19 +116,21 @@ class soma_measure : public Leap::Listener
             if (pw.fullness (f.timestamp ()) > 0.9)
             {
                 clog << "position ";
+                stats (pw, 0);
                 stats (pw);
                 pw.clear ();
             }
             if (vw.fullness (f.timestamp ()) > 0.9)
             {
                 clog << "velocity ";
-                stats (vw);
+                stats (vw, 0);
                 vw.clear ();
             }
             if (dw.fullness (f.timestamp ()) > 0.9)
             {
                 clog << "direction ";
-                stats (dw);
+                stats (dw, 0);
+                stats (dw, 1);
                 dw.clear ();
             }
         }
