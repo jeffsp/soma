e56a7407adc08be34fb4950ab57fdd280f3cef2c
Jeff Perry <jeffsp@gmail.com>
2013-09-23 13:53:49 -0500

    add tests for running stats

--- tests/test_stats.cc
+++ tests/test_stats.cc
@@ -19,11 +19,46 @@ void test_stats (const bool verbose)
         clog << "mode(x)=" << mode (x) << endl;
     VERIFY (mode (x) == 2);
     if (verbose)
-        clog << "average(x)=" << average(x) << endl;
-    VERIFY (average (x) == 3);
+        clog << "mean(x)=" << mean(x) << endl;
+    VERIFY (mean (x) == 3);
     if (verbose)
         clog << "variance(x)=" << variance(x) << endl;
     VERIFY (round (variance (x) * 100) == 523);
+}
+
+void test_running_stats (const bool verbose)
+{
+    running_mean a;
+    a.add (1); a.add (2); a.add (3); a.add (4); a.add (5);
+    if (verbose)
+        clog << "running_mean=" << a.mean () << endl;
+    VERIFY (a.mean () == 3);
+    a.add (6); a.add (7);
+    if (verbose)
+        clog << "running_mean=" << a.mean () << endl;
+    VERIFY (a.mean () == 4);
+    a.remove (1); a.remove (2);
+    if (verbose)
+        clog << "running_mean=" << a.mean () << endl;
+    VERIFY (a.mean () == 5);
+    running_mode b;
+    b.add (7); b.add (1); b.add (3); b.add (4); b.add (3); b.add (3); b.add (7); b.add (9);
+    if (verbose)
+        clog << "running_mode=" << b.mode () << endl;
+    VERIFY (b.mode () == 3);
+    b.add (7); b.add (7);
+    if (verbose)
+        clog << "running_mode=" << b.mode () << endl;
+    VERIFY (b.mode () == 7);
+    // both 3 and 7 are the mode-- the mode shouldn't change
+    b.remove (7);
+    if (verbose)
+        clog << "running_mode=" << b.mode () << endl;
+    VERIFY (b.mode () == 7);
+    b.remove (7);
+    if (verbose)
+        clog << "running_mode=" << b.mode () << endl;
+    VERIFY (b.mode () == 3);
 }
 
 int main (int argc, char **)
@@ -32,6 +67,7 @@ int main (int argc, char **)
     {
         const bool verbose = (argc > 1);
         test_stats (verbose);
+        test_running_stats (verbose);
 
         return 0;
     }
