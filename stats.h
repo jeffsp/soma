fd6b05f59c935d2b06a9800fc8880e58f98acc6a
Jeff Perry <jeffsp@gmail.com>
2013-09-23 13:53:04 -0500

    add running mean and mode helper classes

--- stats.h
+++ stats.h
@@ -8,12 +8,19 @@
 #define STATS_H
 
 #include <algorithm>
+#include <cassert>
 #include <cstdlib>
 #include <unordered_map>
 
 namespace soma
 {
 
+/// @brief get the mode of a container of numbers
+///
+/// @tparam T container type
+/// @param x
+///
+/// @return the mode
 template<typename T>
 typename T::value_type mode (const T &x)
 {
@@ -32,14 +39,26 @@ typename T::value_type mode (const T &x)
     return m;
 }
 
+/// @brief get the mean of a container of numbers
+///
+/// @tparam T container type
+/// @param x
+///
+/// @return the mean
 template<typename T>
-double average (const T &x)
+double mean (const T &x)
 {
     if (x.empty ())
         return 0.0;
     return std::accumulate (x.begin (), x.end (), 0.0) / x.size ();
 }
 
+/// @brief get the variance of a container of numbers
+///
+/// @tparam T container type
+/// @param x
+///
+/// @return the variance
 template<typename T>
 double variance (const T &x)
 {
@@ -55,6 +74,125 @@ double variance (const T &x)
     // var = E[x^2]-E[x]^2
     return (sum2 / x.size ()) - (sum / x.size ()) * (sum / x.size ());
 }
+
+/// @brief keep a running sum and total that you can add and remove numbers from
+class running_mean
+{
+    private:
+    size_t total;
+    double sum;
+    public:
+    /// @brief contructor
+    running_mean ()
+        : total (0)
+        , sum (0)
+    {
+    }
+    /// @brief reset to zero
+    void reset ()
+    {
+        total = 0;
+        sum = 0;
+    }
+    /// @brief add a number to the running sum and total
+    ///
+    /// @param x number to add
+    void add (const double x)
+    {
+        ++total;
+        sum += x;
+    }
+    /// @brief remove a number from the running sum and total
+    ///
+    /// @param x number to remove
+    void remove (const double x)
+    {
+        assert (total > 0);
+        assert (sum - x >= 0);
+        --total;
+        sum -= x;
+    }
+    /// @brief get the current mean
+    ///
+    /// @return the mean
+    double mean () const
+    {
+        return static_cast<double> (sum) / total;
+    }
+};
+
+/// @brief keep a running distribution that you can add and remove numbers from
+class running_mode
+{
+    private:
+    /// @brief the distribution
+    std::unordered_map<int,size_t> d;
+    /// @brief the mode
+    int m;
+    /// @brief count of the mode
+    size_t count;
+    public:
+    /// @brief contructor
+    running_mode ()
+        : count (0)
+    {
+    }
+    /// @brief reset to zero
+    void reset ()
+    {
+        d.clear ();
+        count = 0;
+    }
+    /// @brief add a number to the running distribution
+    ///
+    /// @param x number to add
+    void add (const int x)
+    {
+        // update the distribution
+        ++d[x];
+        // if this count is greater than the mode's count, change the mode
+        if (d[x] > count)
+        {
+            m = x;
+            count = d[x];
+        }
+    }
+    /// @brief remove a number from the running distribution
+    ///
+    /// @param x number to remove
+    void remove (const int x)
+    {
+        assert (!d.empty ());
+        assert (count > 0);
+        assert (d[x] != 0);
+        // update the count
+        --d[x];
+        // if this number was the mode, then the mode may have changed,
+        // otherwise it could not have changed
+        if (m == x)
+        {
+            // x is the mode, so mode's count gets decremented
+            --count;
+            // check all counts
+            for (auto i : d)
+            {
+                if (i.second > count)
+                {
+                    m = i.first;
+                    count = i.second;
+                }
+            }
+        }
+    }
+    /// @brief get the current mode
+    ///
+    /// @return the mode
+    size_t mode () const
+    {
+        assert (!d.empty ());
+        return m;
+    }
+};
 
 }
 
