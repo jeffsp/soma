a2d608a24aafd3beada185254c6e6f2a95b3866f
Jeff Perry <jeffsp@gmail.com>
2013-09-26 21:20:29 -0500

    fix centering on cursor test.

--- usability/usability.cpp
+++ usability/usability.cpp
@@ -4,83 +4,128 @@
 
 #include "usability.h"
 
+/// @brief constructor
+///
+/// @param parent parent widget
 UsabilityDialog::UsabilityDialog(QWidget *parent)
     : QDialog(parent)
 {
+    // seed the random number generator
+    qsrand (QTime::currentTime ().msec ());
+    // take up whole screen
     showFullScreen ();
-
-    QFileInfo fileInfo(".");
-
+    // allocate tabs
     tabWidget = new QTabWidget (this);
     cursorTab = new CursorTab(this);
+    buttonTab = new ButtonTab(this);
+    resultsTab = new ResultsTab(this);
+    // add them to the tab widget
     tabWidget->addTab(cursorTab, tr("Follow the Cursor"));
-    tabWidget->addTab(new ButtonTab(this), tr("Buttons"));
-    tabWidget->addTab(new PerformanceTab(this), tr("Performance"));
-
+    tabWidget->addTab(buttonTab, tr("Buttons"));
+    tabWidget->addTab(resultsTab, tr("Results"));
+    // set focus to client area.  this has to be done after the cursor view is constructed.
     cursorTab->getCursorView ()->setFocus ();
-
-    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
-                                     | QDialogButtonBox::Cancel);
-
+    // add the OK/Cancel buttons
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
     connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
     connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
-
+    // set the layout
     QVBoxLayout *mainLayout = new QVBoxLayout (this);
     mainLayout->setSizeConstraint(QLayout::SetNoConstraint);
     mainLayout->addWidget(tabWidget);
     mainLayout->addWidget(buttonBox);
     setLayout(mainLayout);
-
+    // set the title
     setWindowTitle(tr("Usability Tester"));
 }
 
+/// @brief constructor
+///
+/// @param parent parent widget
 CursorScene::CursorScene (QWidget * parent)
     : QGraphicsScene (parent)
     , testing (false)
-    , text (new QGraphicsTextItem ("Keep the cursor inside of the circle.  Press SPACE to begin."))
+    , text (new QGraphicsTextItem ("Try to keep the cursor inside of the circle.  Press SPACE to begin."))
     , circle (new QGraphicsEllipseItem (0))
 {
+    // set the text font
     text->setFont (QFont ("Arial", 18, QFont::Bold));
-    circle->setRect (-10.0, -10.0, 10.0, 10.0);
+    // center the instruction text
+    QRectF r = text->boundingRect ();
+    text->setPos (-r.width () / 2, -r.height () / 2);
+    // add it
     addItem (text);
+    // make a circle
+    circle->setRect (-RADIUS, -RADIUS, RADIUS, RADIUS);
     addItem (circle);
+    // set background of client area
     setBackgroundBrush (Qt::white);
+    // initially show only the text
     text->setVisible (true);
     circle->setVisible (false);
 }
 
+/// @brief used to move the circle at set intervals
 void CursorScene::timerEvent (QTimerEvent *)
 {
+    // check to see if the test is over yet
+    if (start_time.elapsed () / 1000 >= TEST_SECS)
+    {
+        setTesting (false);
+        return;
+    }
+    // move along a line 'dist' intervals
     static int dist = 0;
     static int dx = 0;
     static int dy = 0;
     if (dist == 0)
     {
+        // we're at the end of the line, get a new one
         dist = qrand () % 10;
+        // get a new direction
         dx = (qrand () % 3) - 1;
         dy = (qrand () % 3) - 1;
     }
     else
     {
+        // count down one interval along the line
         --dist;
     }
-    QPointF p = getCircle ()->pos ();
+    // get the new position
+    QPointF p = circle->pos ();
     QPointF q (p.x () + dx * POS_GAIN, p.y () + dy * POS_GAIN);
+    // remember the position
     posX.push_front (q.x ());
     posY.push_front (q.y ());
+    // forget old positions
     posX.pop_back ();
     posY.pop_back ();
+    // spatial filtering
     float ax = 1.0 * std::accumulate (posX.begin (), posX.end (), 0) / posX.size ();
     float ay = 1.0 * std::accumulate (posY.begin (), posY.end (), 0) / posY.size ();
-    getCircle ()->setPos (QPointF (ax, ay));
+    // set the new position
+    circle->setPos (QPointF (ax, ay));
+    // frame counter
     ++frames;
 };
 
+/// @brief access function
+///
+/// @return testing flag
+bool CursorScene::getTesting () const
+{
+    return testing;
+}
+
+/// @brief access function
+///
+/// @param f testing flag
 void CursorScene::setTesting (bool f)
 {
     testing = f;
     if (testing)
     {
+        // start a new test
         timer.start (1000/30, this);
         start_time.start ();
         frames = 0;
@@ -90,10 +135,10 @@ void CursorScene::setTesting (bool f)
         posY.clear ();
         posX.assign (POS_MAX, 0.0);
         posY.assign (POS_MAX, 0.0);
-        getCircle ()->setPos (QPointF (0.0, 0.0));
     }
     else
     {
+        // end the test
         timer.stop ();
         int elapsed = start_time.elapsed ();
         float secs = elapsed / 1000.0f;
@@ -107,42 +152,52 @@ void CursorScene::setTesting (bool f)
     }
 }
 
+/// @brief constructor
+///
+/// @param scene scene data
+/// @param parent parent widget
 CursorView::CursorView (CursorScene * scene, QWidget * parent)
     : QGraphicsView (scene, parent)
     , cursorScene (scene)
 {
 }
 
+/// @brief constructor
+///
+/// @param parent parent widget
 CursorTab::CursorTab(QWidget *parent)
     : QWidget(parent)
 {
+    // setup the scene and view
     cursorScene = new CursorScene (this);
     cursorView = new CursorView (cursorScene, this);
     cursorView->setRenderHints (QPainter::Antialiasing);
     cursorView->show ();
+    // setup the layout
     QHBoxLayout *mainLayout = new QHBoxLayout (this);
     mainLayout->addWidget(cursorView);
     setLayout(mainLayout);
 }
 
-void CursorView::paintEvent (QPaintEvent *e)
-{
-    QGraphicsView::paintEvent (e);
-}
-
+/// @brief control the cursor test with the keyboard
+///
+/// @param e keyboard event
 void CursorTab::keyPressEvent (QKeyEvent *e)
 {
-    qDebug () << "CursorTab::keyPressEvent" << e->key ();
     switch (e->key())
     {
+        // on SPACEBAR, start the test
         case ' ':
-            qDebug () << "SPACE";
-            cursorScene->setTesting (!cursorScene->getTesting ());
+        cursorScene->setTesting (!cursorScene->getTesting ());
         break;
     }
+    // call through to base class
     QWidget::keyPressEvent (e);
 }
 
+/// @brief constructor
+///
+/// @param parent parent widget
 ButtonTab::ButtonTab(QWidget *parent)
     : QWidget(parent)
 {
@@ -153,7 +208,10 @@ ButtonTab::ButtonTab(QWidget *parent)
     setLayout(mainLayout);
 }
 
-PerformanceTab::PerformanceTab(QWidget *parent)
+/// @brief constructor
+///
+/// @param parent parent widget
+ResultsTab::ResultsTab(QWidget *parent)
     : QWidget(parent)
 {
     QHBoxLayout *mainLayout = new QHBoxLayout;
--- usability/usability.h
+++ usability/usability.h
@@ -4,93 +4,131 @@
 #include <deque>
 #include <QtGui>
 
-QT_BEGIN_NAMESPACE
-class QDialogButtonBox;
-class QFileInfo;
-class QTabWidget;
-QT_END_NAMESPACE
-
+/// @brief scene data for the cursor test
 class CursorScene : public QGraphicsScene
 {
+    Q_OBJECT
+    /// @brief duration of test
+    static const int TEST_SECS = 5;
+    /// @brief size of circle
+    static const int RADIUS = 30;
+    /// @brief used to filter circle position
     std::deque<int> posX;
     std::deque<int> posY;
+    /// @brief movement/filter parameters
     static const int POS_GAIN = 40;
     static const size_t POS_MAX = 17;
+    /// @brief flag that says if we are currently running a test
     bool testing;
+    /// @brief test instructions
     QGraphicsTextItem *text;
+    /// @brief displayed circle
     QGraphicsEllipseItem *circle;
+    /// @brief used to generate circle movement events
     QBasicTimer timer;
+    /// @brief for timing
     QTime start_time;
+    /// @brief frame counter
     int frames;
-public:
+    public:
+    /// @brief constructor
+    ///
+    /// @param parent parent widget
     CursorScene (QWidget * parent = 0);
+    /// @brief override
+    ///
+    /// @param event
     void timerEvent (QTimerEvent* event);
-    QGraphicsTextItem *getText () const
-    {
-        return text;
-    }
-    QGraphicsEllipseItem *getCircle () const
-    {
-        return circle;
-    }
-    bool getTesting () const
-    {
-        return testing;
-    }
+    /// @brief testing flag access
+    ///
+    /// @return testing flag
+    bool getTesting () const;
+    /// @brief testing flag access
+    ///
+    /// @param f testing flag
     void setTesting (bool f);
 };
 
+/// @brief view for cursor scene data
 class CursorView : public QGraphicsView
 {
+    Q_OBJECT
+    /// @brief scene data
     CursorScene *cursorScene;
-public:
-    CursorView (CursorScene * scene, QWidget * parent = 0);
-    void paintEvent (QPaintEvent *);
-    CursorScene *scene () { return cursorScene; }
+    public:
+    /// @brief constructor
+    ///
+    /// @param scene scene data
+    /// @param parent parent widget
+    CursorView (CursorScene *scene, QWidget * parent = 0);
 };
 
+/// @brief cursor test tab
 class CursorTab : public QWidget
 {
     Q_OBJECT
+    /// @brief scene data
     CursorScene *cursorScene;
+    /// @brief scene view
     CursorView *cursorView;
-
-public:
+    public:
+    /// @brief constructor
+    ///
+    /// @param parent parent widget
     CursorTab(QWidget *parent = 0);
+    /// @brief override
+    ///
+    /// @param control tests
     void keyPressEvent (QKeyEvent *);
-    CursorScene *getCursorScene () const { return cursorScene; }
+    /// @brief view access
+    ///
+    /// @return cursor view
+    ///
+    /// Required for setting focus AFTER the tab is visible.
     CursorView *getCursorView () const { return cursorView; }
 };
 
+/// @brief button test tab
 class ButtonTab : public QWidget
 {
     Q_OBJECT
-
-public:
+    public:
+    /// @brief constructor
+    ///
+    /// @param parent widget
     ButtonTab(QWidget *parent = 0);
 };
 
 
-class PerformanceTab : public QWidget
+/// @brief test results tab
+class ResultsTab : public QWidget
 {
     Q_OBJECT
-
-public:
-    PerformanceTab(QWidget *parent = 0);
+    public:
+    /// @brief constructor
+    ///
+    /// @param parent widget
+    ResultsTab(QWidget *parent = 0);
 };
 
+/// @brief dialog that contains usability test tabs
 class UsabilityDialog : public QDialog
 {
     Q_OBJECT
-
-    CursorTab *getCursorTab () const { return cursorTab; }
-public:
+    public:
+    /// @brief constructor
+    ///
+    /// @param parent widget
     UsabilityDialog(QWidget *parent = 0);
-
-private:
+    private:
+    /// @brief widget that contains the tabs
     QTabWidget *tabWidget;
+    /// @brief OK/Cancel buttons
     QDialogButtonBox *buttonBox;
+    /// @brief tabs
     CursorTab *cursorTab;
+    ButtonTab *buttonTab;
+    ResultsTab *resultsTab;
 };
 
 #endif
