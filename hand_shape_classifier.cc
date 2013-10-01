e728974e4e7f529885bc317faedf51240449620e
Jeff Perry <jeffsp@gmail.com>
2013-10-01 10:49:55 -0500

    simplify: remove naive bayes classifier

---
+++ hand_shape_classifier.cc
@@ -0,0 +1,65 @@
+/// @file hand_shape_classifier.cc
+/// @brief classify hand shapes
+/// @author Jeff Perry <jeffsp@gmail.com>
+/// @version 1.0
+/// @date 2013-09-20
+
+#include <stdexcept>
+#include "soma.h"
+
+using namespace std;
+using namespace soma;
+const string usage = "usage: hand_shape_classifier";
+
+class grabber : public Leap::Listener
+{
+    private:
+    hand_shape_classifier hsc;
+    public:
+    grabber (uint64_t duration)
+        : hsc (duration)
+    {
+    }
+    virtual void onFrame (const Leap::Controller& c)
+    {
+        // get the frame
+        Leap::Frame f = c.frame ();
+        uint64_t ts = f.timestamp ();
+        // get the sample
+        hand_sample s (f.pointables ());
+        // add it to the classifier
+        hsc.add (ts, s);
+        // if it's changed, print the result
+        if (hsc.has_changed ())
+            clog << to_string (hsc.get_shape ()) << endl;
+    }
+};
+
+int main (int argc, char **argv)
+{
+    try
+    {
+        if (argc != 1)
+            throw runtime_error (usage);
+
+        clog << "Press CTRL-C to exit" << endl;
+
+        static const uint64_t WINDOW_DURATION = 400000;
+        grabber g (WINDOW_DURATION);
+        Leap::Controller c (g);
+
+        // receive frames even when you don't have focus
+        c.setPolicyFlags (Leap::Controller::POLICY_BACKGROUND_FRAMES);
+
+        // loop
+        while (1)
+            usleep (5000);
+
+        return 0;
+    }
+    catch (const exception &e)
+    {
+        cerr << e.what () << endl;
+        return -1;
+    }
+}
--- hand_shape_classifier.h
+++ hand_shape_classifier.h
@@ -8,209 +8,95 @@
 #define HAND_SHAPE_CLASSIFIER_H
 
 #include "hand_sample.h"
-#include <limits>
+#include "finger_counter.h"
+#include <string>
 
 namespace soma
 {
 
-size_t hand_shape_dimensions (size_t fingers)
+enum class hand_shape
 {
-    if (fingers == 0)
-        return 1;
-    else
-        return 1 + fingers * 3 + (fingers - 1) * 4;
+    unknown = -1,
+    zero = 0,
+    pointing = 1,
+    clicking = 2,
+    scrolling = 3,
+    ok = 4,
+    center = 5,
+};
+
+std::string to_string (const hand_shape s)
+{
+    switch (s)
+    {
+        default: assert (0); // logic error
+        case hand_shape::unknown: return std::string ("unknown");
+        case hand_shape::zero: return std::string ("zero");
+        case hand_shape::pointing: return std::string ("pointing");
+        case hand_shape::clicking: return std::string ("clicking");
+        case hand_shape::scrolling: return std::string ("scrolling");
+        case hand_shape::ok: return std::string ("ok");
+        case hand_shape::center: return std::string ("center");
+    }
 }
-
-class hand_shape_features : public std::vector<double>
-{
-    public:
-    hand_shape_features (const hand_sample &h)
-    {
-        size_t fingers = h.size ();
-        push_back (fingers);
-        for (size_t i = 0; i < h.size (); ++i)
-        {
-            push_back (h[i].direction.x);
-            push_back (h[i].direction.y);
-            push_back (h[i].direction.z);
-            if (i + 1 < h.size ())
-            {
-                float dist = h[i].position.distanceTo (h[i + 1].position);
-                push_back (dist);
-                vec3 dir = h[i].position - h[i + 1].position;
-                push_back (dir.x);
-                push_back (dir.y);
-                push_back (dir.z);
-            }
-        }
-        assert (size () == hand_shape_dimensions (fingers));
-    }
-};
-
-class hand_shape_feature_dist
-{
-    private:
-    size_t total;
-    std::vector<double> u1;
-    std::vector<double> u2;
-    public:
-    void resize (size_t n)
-    {
-        u1.resize (n);
-        u2.resize (n);
-    }
-    size_t size () const
-    {
-        assert (u1.size () == u2.size ());
-        return u1.size ();
-    }
-    void update (size_t i, const double v)
-    {
-        assert (i < u1.size ());
-        assert (u1.size () == u2.size ());
-        ++total;
-        u1[i] += v;
-        u2[i] += v * v;
-    }
-    double mean (size_t i) const
-    {
-        assert (i < u1.size ());
-        if (total == 0)
-            return 0.0;
-        return u1[i] / total;
-    }
-    double variance (size_t i) const
-    {
-        assert (i < u2.size ());
-        if (total == 0)
-            return 0.0;
-        double u = mean (i);
-        assert (u2[i] / total >= u * u);
-        return u2[i] / total - u * u;
-    }
-    friend std::ostream& operator<< (std::ostream &s, const hand_shape_feature_dist &x)
-    {
-        s << x.total << std::endl;
-        assert (x.u1.size () == x.u2.size ());
-        s << x.u1.size () << std::endl;
-        s.precision (12);
-        for (size_t i = 0; i < x.u1.size (); ++i)
-            s << ' ' << x.u1[i] << ' ' << x.u2[i];
-        return s;
-    }
-    friend std::istream& operator>> (std::istream &s, hand_shape_feature_dist &x)
-    {
-        s >> x.total;
-        size_t n;
-        s >> n;
-        x.resize (n);
-        for (size_t i = 0; i < x.u1.size (); ++i)
-            s >> x.u1[i] >> x.u2[i];
-        return s;
-    }
-};
-
-typedef int hand_shape;
-const std::vector<hand_shape> hand_shapes { 0, 1, 2, 3, 4 };
 
 class hand_shape_classifier
 {
     private:
-    typedef std::map<hand_shape,hand_shape_feature_dist> map_hand_shape_dists;
-    std::vector<map_hand_shape_dists> hss;
+    finger_counter fc;
+    hand_shape current;
+    bool changed;
+    void update ()
+    {
+        if (fc.has_changed ())
+        {
+            switch (fc.get_count ())
+            {
+                default:
+                    current = hand_shape::unknown;
+                break;
+                case 0:
+                    current = hand_shape::zero;
+                break;
+                case 1:
+                    current = hand_shape::pointing;
+                break;
+                case 2:
+                    current = hand_shape::scrolling;
+                break;
+                case 3:
+                    current = hand_shape::ok;
+                break;
+                case 4:
+                    current = hand_shape::unknown;
+                break;
+                case 5:
+                    current = hand_shape::center;
+                break;
+            }
+        }
+    }
     public:
-    hand_shape_classifier ()
-        : hss (5)
+    hand_shape_classifier (uint64_t duration)
+        : fc (duration)
+        , current (hand_shape::unknown)
+        , changed (false)
     {
-        // for each number of fingers
-        for (size_t i = 0; i < hss.size (); ++i)
-            // for each hand shape
-            for (auto hs : hand_shapes)
-                // resize the vector of stats to the number of dimensions
-                hss[i][hs].resize (hand_shape_dimensions (i + 1));
     }
-    void update (const hand_shape hs, const hand_shape_features &hsf)
+    void add (uint64_t ts, const hand_sample &s)
     {
-        // first dimension always contains the number of fingers
-        const size_t fingers = hsf[0];
-        // if there are no fingers detected, there is nothing to do
-        if (fingers == 0)
-            return;
-        const size_t map_index = fingers - 1;
-        assert (map_index < hss.size ());
-        auto &v = hss[map_index][hs];
-        //std::clog << "hand shape feature vector dimensions " << s.size () << std::endl;
-        //std::clog << "hand shape dists vector dimensions " << v.size () << std::endl;
-        assert (hsf.size () == v.size ());
-        for (size_t i = 0; i < hsf.size (); ++i)
-        {
-            v.update (i, hsf[i]);
-            double m = v.mean (i);
-            double s = v.variance (i);
-            std::clog << i << ' ' << m << ' ' << sqrt (s) << std::endl;
-        }
+        hand_shape last = current;
+        fc.add (ts, s.size ());
+        update ();
+        changed = (last != current);
     }
-    template<typename T>
-    hand_shape classify (const T beg, const T end, std::map<hand_shape,double> &l) const
+    hand_shape get_shape () const
     {
-        for (T s = beg; s != end; ++s)
-        {
-            // first dimension always contains the number of fingers
-            const size_t fingers = (*s)[0];
-            // if there are no fingers detected, there is nothing to do
-            if (fingers == 0)
-                return -1;
-            for (auto hs : hand_shapes)
-            {
-                const size_t map_index = fingers - 1;
-                assert (map_index < hss.size ());
-                auto d = hss[map_index].find (hs);
-                assert (d != hss[map_index].end ());
-                assert ((*s).size () == d->second.size ());
-                for (size_t i = 1; i < (*s).size (); ++i)
-                {
-                    const double x = (*s)[i]; // feature dimension value
-                    const double m = d->second.mean (i);
-                    const double s2 = d->second.variance (i);
-                    // update log likelihood
-                    if (s2 != 0.0)
-                        l[hs] -= (x - m) * (x - m) / (2 * s2);
-                }
-            }
-        }
-        // classify them
-        double best_value = -std::numeric_limits<double>::max ();
-        hand_shape best_hs = -1;
-        for (auto i : l)
-        {
-            if (i.second > best_value)
-            {
-                best_hs = i.first;
-                best_value = i.second;
-            }
-        }
-        return best_hs;
-
+        return current;
     }
-    friend std::ostream& operator<< (std::ostream &s, const hand_shape_classifier &h)
+    bool has_changed () const
     {
-        for (auto i : h.hss)
-            for (auto j : i)
-                s << j.second << std::endl;
-        return s;
-    }
-    friend std::istream& operator>> (std::istream &s, hand_shape_classifier &h)
-    {
-        for (size_t i = 0; i < h.hss.size (); ++i)
-        {
-            for (auto hs : hand_shapes)
-            {
-                hand_shape_feature_dist x;
-                s >> x;
-                h.hss[i][hs] = x;
-            }
-        }
-        return s;
+        return changed;
     }
 };
 
