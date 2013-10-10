7d913673a3c1a5e84309fb968c8d924f9d26a9a7
Jeff Perry <jeffsp@gmail.com>
2013-10-10 16:38:52 -0500

    joystick mode does not work well.  implement flicking mode.

--- mouse_pointer.h
+++ mouse_pointer.h
@@ -15,11 +15,12 @@
 namespace soma
 {
 
+template<typename T>
 struct vec2
 {
-    vec2 () : x (0.0), y (0.0) { }
-    vec2 (const vec3 &p) : x (p.x), y (p.y) { }
-    float x, y;
+    T x, y;
+    vec2 (T x, T y) : x (x), y (y) { }
+    vec2 () : x (0), y (0) { }
 };
 
 // convert from mm to pixels at 96dpi
@@ -28,45 +29,74 @@ double mm_to_pixels (double mm)
     return mm * 3.779527559;
 }
 
+class point_delta
+{
+    private:
+    bool valid;
+    vec2<double> last_p;
+    vec2<double> current_p;
+    uint64_t last_t;
+    uint64_t current_t;
+    public:
+    point_delta ()
+        : valid (false)
+    { }
+    void update (uint64_t ts, const double x, const double y)
+    {
+        if (valid)
+        {
+            last_p = current_p;
+            last_t = current_t;
+        }
+        else
+        {
+            last_p = vec2<double> (x, y);
+            last_t = ts;
+        }
+        current_p = vec2<double> (x, y);
+        current_t = ts;
+        valid = true;
+    }
+    double dx () const
+    {
+        return current_p.x - last_p.x;
+    }
+    double dy () const
+    {
+        return current_p.y - last_p.y;
+    }
+    double dr () const
+    {
+        return sqrt (dx () * dx () + dy () * dy ());
+    }
+    uint64_t dt () const
+    {
+        assert (current_t >= last_t);
+        return current_t - last_t;
+    }
+    void reset ()
+    {
+        valid = false;
+    }
+};
+
 class mouse_pointer
 {
     private:
     static const uint64_t SW_DURATION = 50000;
-    static const unsigned RADIUS = 50;
-    sliding_window<int> swx;
-    sliding_window<int> swy;
+    sliding_window<double> swx;
+    sliding_window<double> swy;
     running_mean smooth_x;
     running_mean smooth_y;
     mouse &m;
     double speed;
-    bool last_valid;
-    vec2 last;
-    bool centered;
-    vec2 cp;
-    void update_normal (uint64_t ts, const vec2 &pos)
-    {
-        swx.add (ts, pos.x, smooth_x);
-        swy.add (ts, pos.y, smooth_y);
-        double x = smooth_x.get_mean ();
-        double y = smooth_y.get_mean ();
-        if (last_valid)
-        {
-            double px = mm_to_pixels (last.x - x);
-            double py = mm_to_pixels (last.y - y);
-            m.move (-px * speed, py * speed);
-        }
-        last_valid = true;
-        last.x = x;
-        last.y = y;
-    }
+    point_delta delta;
     public:
     mouse_pointer (mouse &m, double speed)
         : swx (SW_DURATION)
         , swy (SW_DURATION)
         , m (m)
         , speed (speed)
-        , last_valid (false)
-        , centered (false)
     {
     }
     void set_speed (double s)
@@ -80,44 +110,35 @@ class mouse_pointer
         swy.clear ();
         smooth_x.reset ();
         smooth_y.reset ();
-        last_valid = false;
+        delta.reset ();
     }
-    void update (uint64_t ts, const vec2 &pos)
+    void update (uint64_t ts, const vec3 &pos)
     {
-        double dx = cp.x - pos.x;
-        double dy = cp.y - pos.y;
-        double r = sqrt (dx * dx + dy * dy);
-        if (r < RADIUS || !centered)
+        swx.add (ts, pos.x, smooth_x);
+        swy.add (ts, pos.y, smooth_y);
+        double x = smooth_x.get_mean ();
+        double y = smooth_y.get_mean ();
+        delta.update (ts, x, y);
+        // get mm per second
+        double mmps = 1000000 * delta.dr () / delta.dt ();
+        // did it move fast or slow?
+        double px;
+        double py;
+        const int FAST = 200;
+        if (mmps < FAST)
         {
-            // normal mode
-            update_normal (ts, pos);
+            // slow
+            px = mm_to_pixels (delta.dx ()) * speed;
+            py = mm_to_pixels (-delta.dy ()) * speed;
         }
         else
         {
-            // joystick mode
-            if (r < RADIUS * 1.2)
-                return;
-            if (!last_valid)
-                update_normal (ts, pos);
-            else
-            {
-                double theta = atan2 (dy, dx);
-                double x = 10.0 * cos (theta);
-                double y = 10.0 * sin (theta);
-                std::clog << theta << " " << x << " " << y << std::endl;
-                vec2 tmp;
-                tmp.x = last.x - x;
-                tmp.y = last.y - y;
-                update_normal (ts, tmp);
-            }
+            // fast
+            double gain = (mmps - FAST) / 100;
+            px = gain * mm_to_pixels (delta.dx ()) * speed;
+            py = gain * mm_to_pixels (-delta.dy ()) * speed;
         }
-        // else do nothing
-    }
-    void center (const vec2 &pos)
-    {
-        centered = !centered;
-        std::clog << "center " << (centered ? "ON" : "OFF") << std::endl;
-        cp = pos;
+        m.move (px, py);
     }
 };
 
